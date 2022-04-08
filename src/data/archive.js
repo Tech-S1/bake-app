cdcf00b22c69c508be599abd8b2466fb2fadd990
Tech-S1 <74782108+Tech-S1@users.noreply.github.com>
2022-04-08 22:56:18 +0100

    feat: update to add archive page

---
+++ src/data/archive.js
@@ -0,0 +1,19 @@
+const archive = [
+  {
+    date: "2020/11/01",
+    title: "Tray Bake",
+    scores: [
+      { bakerId: 1, name: "Zach", appearance: 4, taste: 2 },
+      { bakerId: 2, name: "Bob", appearance: 3, taste: 5 },
+    ],
+  },
+  {
+    date: "2020/11/02",
+    title: "Cheese Cake",
+    scores: [
+      { bakerId: 1, name: "Harry", appearance: 3, taste: 3 },
+      { bakerId: 2, name: "Callum", appearance: 1, taste: 5 },
+    ],
+  },
+];
+export default archive;
--- src/pages/ArchivePage.jsx
+++ src/pages/ArchivePage.jsx
@@ -1,8 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DefaultLayout from "../containers/DefaultLayout";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import archive from "../data/archive";
+import Table from "../components/Table";
+
+const columns = [
+  { title: "Baker Id", field: "bakerId", type: "numeric" },
+  { title: "Baker Name", field: "name" },
+  {
+    title: "Appearance Score ",
+    field: "appearance",
+    type: "numeric",
+  },
+  {
+    title: "Taste Score ",
+    field: "taste",
+    type: "numeric",
+  },
+  {
+    field: "total",
+    title: "Total Score",
+    type: "numeric",
+  },
+];
 
 const ArchivePage = () => {
-  return <DefaultLayout>Archive Page</DefaultLayout>;
+  const [date, setDate] = useState();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    //TODO: Get archive data (API Call)
+    setData(archive);
+  }, []);
+
+  return (
+    <DefaultLayout>
+      <Box
+        display="flex"
+        width="100%"
+        height={100}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Box
+          display="flex"
+          width="50%"
+          height={100}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Competition</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={date}
+              label="Competition"
+              onChange={({ target }) => setDate(target.value)}
+            >
+              {data.map((e) => (
+                <MenuItem value={e.date}>{`${e.date} - ${e.title}`}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+      </Box>
+      {date && (
+        <Table
+          title="Scores"
+          columns={columns}
+          data={data
+            .filter((e) => e.date === date)[0]
+            .scores.map((item) => ({
+              ...item,
+              total: item.appearance + item.taste,
+            }))}
+        />
+      )}
+    </DefaultLayout>
+  );
 };
 
 export default ArchivePage;
