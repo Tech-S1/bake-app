c0e040c33a5ad38b7a3948d8895e1b75c6037bde
Callum Nightingale <callum.nightingale@capgemini.com>
2022-04-14 17:20:26 +0100

    refactor: change bakeoff creation endpoint

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ const qrEnabled = process.env.REACT_APP_QR;
 const baseurl = process.env.REACT_APP_BASE_URL;
 const baseEndpoint = "/bakeoff";
 const latestEndpoint = `${baseEndpoint}/latest`;
+const bakeoffEndpoint = `${baseEndpoint}/event`
 const judgeEndpoint = `${baseEndpoint}/judge`;
 const bakerEndpoint = `${baseEndpoint}/baker`;
 const participantEndpoint = `${baseEndpoint}/participant`;
@@ -17,4 +18,5 @@ export {
   bakerEndpoint,
   participantEndpoint,
   resultEndpoint,
+  bakeoffEndpoint,
 };
